9ba39c1a06c1e1be005053931325cd8ea2d2ca0c
William Johnson <catfishfishcat10101@gmail.com>
2025-08-20 11:49:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser, clearUser } from "./redux/userSlice";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import MainGame from "./components/UI/MainGame";
 import Splash from "./components/UI/Splash";
+import NavBar from "./components/UI/NavBar";
 
-function App() {
-  const loggedIn = useSelector((state) => state.user.loggedIn);
+function ProtectedRoute({ children }) {
+  const loggedIn = useSelector((s) => s.user.loggedIn);
+  return loggedIn ? children : <Navigate to="/doggerz/login" replace />;
+}
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        dispatch(setUser({ uid: user.uid, email: user.email }));
-      } else {
-        dispatch(clearUser());
-      }
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) dispatch(setUser({ uid: user.uid, email: user.email }));
+      else dispatch(clearUser());
+      setLoading(false);
     });
-
-    return () => unsubscribe();
+    return unsub;
   }, [dispatch]);
 
+  if (loading) return <div style={{ color: "#fff", padding: "2rem" }}>Loading…</div>;
+
   return (
-    <Routes>
-      {/* Redirect from root to splash */}
-      <Route index element={<Navigate to="/doggerz" replace />} />
-
-      {/* Public routes */}
-      <Route path="/doggerz" element={<Splash />} />
-      <Route path="/doggerz/login" element={<Login />} />
-      <Route path="/doggerz/signup" element={<Signup />} />
-
-      {/* Protected route */}
-      <Route
-        path="/doggerz/game"
-        element={
-          loggedIn ? <MainGame /> : <Navigate to="/doggerz/login" replace />
-        }
-      />
-
-      {/* Fallback route */}
-      <Route path="*" element={<Navigate to="/doggerz" replace />} />
-    </Routes>
+    <HelmetProvider>
+      <NavBar />
+      <Routes>
+        <Route index element={<Navigate to="/doggerz" replace />} />
+        <Route path="/doggerz" element={<Splash />} />
+        <Route path="/doggerz/login" element={<Login />} />
+        <Route path="/doggerz/signup" element={<Signup />} />
+        <Route path="/doggerz/game" element={
+          <ProtectedRoute><MainGame /></ProtectedRoute>
+        }/>
+        <Route path="*" element={<Navigate to="/doggerz" replace />} />
+      </Routes>
+    </HelmetProvider>
   );
 }
-
-export default App;
