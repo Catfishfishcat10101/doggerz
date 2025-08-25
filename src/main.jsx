3eca0fee6274e4fa71b23dec58ddc8895ea9aab3
William Johnson <catfishfishcat10101@gmail.com>
2025-08-25 04:19:03 -0500

    g

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-// src/main.jsx
-import { renderApp } from "./index.jsx";
-
-renderApp();
-
-// HMR: re-render without losing state while editing
-if (import.meta.hot) {
-  import.meta.hot.accept(() => renderApp());
-}
