0cf792ae1b2ea0ebe0bb1f406df55fc6f6683017
William Johnson <catfishfishcat10101@gmail.com>
2025-09-01 16:58:55 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  // DO NOT set "root" here; default is project root (where index.html lives)
-  // base: "/"  // fine for Firebase at site root
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+    },
+  },
+  base: "/",
 });
