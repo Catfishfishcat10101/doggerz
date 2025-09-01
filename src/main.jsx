ce91f356fee220872726a8f31a65c1243d6769ed
William Johnson <catfishfishcat10101@gmail.com>
2025-09-01 17:03:49 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
 // src/main.jsx
 import { renderApp } from "./index.js";
 renderApp();
+
+// (bonus) If your index.html points at /src/main.jsx, keep this tiny handoff:
+// src/main.jsx
+import { renderApp } from "./index.js";
+renderApp();
