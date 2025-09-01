76d295f703ea4e061af5a23c03ca013d859997bb
William Johnson <catfishfishcat10101@gmail.com>
2025-09-01 16:51:23 -0500

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,23 @@
 // src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
+import dogReducer from "./dogSlice.js";
+import userReducer from "./userSlice.js";
 
-// minimal placeholder store so Provider works. Add real reducers later.
-export default configureStore({
-  reducer: {},
+/** Global Redux store */
+export const store = configureStore({
+  reducer: {
+    dog: dogReducer,
+    user: userReducer,
+  },
+  devTools: import.meta.env.MODE !== "production",
 });
+
+// Optional helpers (uncomment if you want handy types via JSDoc in JS projects):
+/**
+ * @returns {import('@reduxjs/toolkit').EnhancedStore['getState']}
+ */
+// export const getState = () => store.getState();
+/**
+ * @type {import('@reduxjs/toolkit').EnhancedStore['dispatch']}
+ */
+// export const dispatch = store.dispatch;
