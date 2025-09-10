e78c0b302754577f993e78c6f2aeff0675ff64a9
William Johnson <catfishfishcat10101@gmail.com>
2025-09-10 02:59:15 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import AuthProvider from "./context/AuthProvider.jsx"; // <-- no braces
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
