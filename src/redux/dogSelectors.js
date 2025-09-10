9eb2a42b9e695466b1562b5b99c1ac6552172532
William Johnson <catfishfishcat10101@gmail.com>
2025-09-10 03:30:08 -0500

    Update dogSelectors.js

--- src/redux/dogSelectors.js
+++ src/redux/dogSelectors.js
@@ -1,8 +1,3 @@
-// src/redux/dogSelectors.js
-export const selectDog = (s) => s.dog;
-export const selectDogState = (s) => ({
-happiness: s.dog.hapiness,
-energy: s.dog.energy,
-hunger: s.dog.hunger,
-});
-export const selectDogLevel = (s) => s.dog.level ?? 1;
+export const selectDog = (state) => state.dog;
+export const selectDogLevel = (state) => state.dog.level;
+export const selectDogNeeds = (state) => state.dog.needs;
