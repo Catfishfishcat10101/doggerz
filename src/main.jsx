fea68f6bdcac8b4ca160b7dd31f073ca9a31f208
William Johnson <catfishfishcat10101@gmail.com>
2025-09-10 03:24:33 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import AuthProvider from "./context/AuthProvider.jsx"; // <-- no braces
-import App from "./App.jsx";
-import "./styles/index.css";
+import { BrowserRouter } from "react-router-dom";
+import store from "@redux/store";
+import "@styles/index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AuthProvider>
+      <BrowserRouter>
         <App />
-      </AuthProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
