377f977cba94f47d7833c817299e89d18409b2e9
William Johnson <catfishfishcat10101@gmail.com>
2025-09-10 03:22:10 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-// ./post.config.js
-import tailwindPostcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    tailwindPostcss(),
-    autoprefixer(),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
