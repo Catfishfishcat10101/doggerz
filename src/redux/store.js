9d5446ff4a244eb83a1ee0743538ccfa5011ef4e
William Johnson <catfishfishcat10101@gmail.com>
2025-09-10 03:27:20 -0500

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import user from "./userSlice";
 import dog from "./dogSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: { user, dog },
-  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefault) => getDefault({ serializableCheck: false }),
 });
+export default store;
