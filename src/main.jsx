a1eea1a705a85e83d02793e24298b69edc35e822
William Johnson <catfishfishcat10101@gmail.com>
2025-09-27 05:39:35 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Use BrowserRouter in dev, HashRouter in built preview to avoid history/base issues
+const Router = import.meta.env.DEV ? BrowserRouter : HashRouter;
+
+// Do NOT register a service worker during local preview; stale caches cause “blank screen”.
+const rootEl = document.getElementById("root");
+ReactDOM.createRoot(rootEl).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
