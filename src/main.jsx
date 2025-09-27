3a23cab8a0b8ccc1b07bba1979865ee9eb8159d9
William Johnson <catfishfishcat10101@gmail.com>
2025-09-27 08:06:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import store from "./redux/store.js";
+import { store } from "./redux/store.js";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true
+      }}>
         <App />
       </BrowserRouter>
     </Provider>
