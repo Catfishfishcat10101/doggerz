18a89b9c216e9952c251e004dcf6ec7036f1ea78
William Johnson <catfishfishcat10101@gmail.com>
2025-11-07 10:36:07 -0600

    Update RootLayout.jsx

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,24 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import FirebaseAutoSave from "./FirebaseAutoSave";
+import InstallPrompt from "./InstallPrompt";
 
 export default function RootLayout() {
   return (
-    <div className="p-4">
-      <Outlet />
-    </div>
+    <>
+      <ErrorBoundary>
+        <NavBar />
+        <main className="min-h-[calc(100dvh-3.5rem-3rem)] bg-[#0b1020]">
+          {/* Autosave while the app runs */}
+          <FirebaseAutoSave />
+          {/* PWA CTA */}
+          <InstallPrompt />
+          <Outlet />
+        </main>
+        <Footer />
+      </ErrorBoundary>
+    </>
   );
 }
