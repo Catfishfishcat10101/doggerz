e00c3c3734cc2665dfb9b6a29643dc3cfaa6651f
William Johnson <catfishfishcat10101@gmail.com>
2025-11-12 14:56:40 -0600

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 // src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
-import dog from "./dogSlice";
+import dogReducer from "./dogSlice.js";
+
+// If dogSlice.js doesn’t exist yet, we’ll wire it next.
+// For now we assume your dog state lives in redux/dogSlice.js
 
 const store = configureStore({
-  reducer: { dog },
-  devTools: true,
+  reducer: {
+    dog: dogReducer,
+  },
+  // You can add more slices here later, e.g. ui: uiReducer
 });
 
 export default store;
