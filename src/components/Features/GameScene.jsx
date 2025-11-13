7b348f5fd4cf7bdf29717afcf16d1211fff74a0b
William Johnson <catfishfishcat10101@gmail.com>
2025-11-12 18:25:30 -0600

    Create GameScene.jsx

---
+++ src/components/Features/GameScene.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BackgroundScene from "@/components/Features/BackgroundScene.jsx";
+import DogSpriteView from "@/components/Features/DogSpriteView.jsx";
+import DogAIEngine from "@/components/Features/DogAIEngine.jsx";
+
+export default function GameScene() {
+  return (
+    <div className="relative w-[480px] h-[320px] mx-auto">
+      <div className="absolute inset-0"><BackgroundScene /></div>
+      <div className="absolute inset-0"><DogSpriteView /></div>
+      <DogAIEngine />
+    </div>
+  );
+}
