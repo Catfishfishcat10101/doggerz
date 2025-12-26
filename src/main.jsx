37921e187513da548425de6586b915f0179db78f
William Johnson <youremail@example.com>
2025-12-25 23:03:52 -0600

    Clean up game feature duplicates; add global toasts; upgrade game UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,29 @@
-import "./App.css";
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-
 import App from "./App.jsx";
 import store from "@/redux/store.js";
-
+import { ToastProvider } from "@/components/toast/ToastProvider.jsx";
 
 import "./index.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter
-        future={{
-          v7_startTransition: true,
-          v7_relativeSplatPath: true,
-        }}
-      >
-        <App />
-      </BrowserRouter>
+      <ToastProvider>
+        <BrowserRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
+          <App />
+        </BrowserRouter>
+      </ToastProvider>
     </Provider>
   </React.StrictMode>,
 );
